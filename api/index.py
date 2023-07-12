22a290f1b61f54f76a426a0c690fa145a53c791d
Vegard Harper <vegard.harper@folq.no>
2023-07-12 10:57:16 +0200

    Test with smaller embeddings-model

--- api/index.py
+++ api/index.py
@@ -1,10 +1,19 @@
 from flask import Flask
+from sentence_transformers import SentenceTransformer
 
 app = Flask(__name__)
+model = SentenceTransformer('paraphrase-albert-small-v2 ')
 #Our sentences we like to encode
+sentence = ['This framework generates embeddings for each input sentence']
+
+#Sentences are encoded by calling model.encode()
+embeddings = model.encode(sentence)
+
 
 @app.route("/api/python")
 def hello_world():
     return {
         "Test": "Test",
+        "sentences": sentence,
+        "embeddings": embeddings[0].tolist()
     }
