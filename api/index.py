18249fb0bfbce4901cdc7d92a93eb0467ec0b479
Vegard Harper <vegard.harper@folq.no>
2023-07-12 10:53:01 +0200

    Test if deployment to vercel works without sentence-transformers

--- api/index.py
+++ api/index.py
@@ -1,19 +1,10 @@
 from flask import Flask
-from sentence_transformers import SentenceTransformer
 
 app = Flask(__name__)
-model = SentenceTransformer('all-MiniLM-L6-v2')
 #Our sentences we like to encode
-sentence = ['This framework generates embeddings for each input sentence']
-
-#Sentences are encoded by calling model.encode()
-embeddings = model.encode(sentence)
-
 
 @app.route("/api/python")
 def hello_world():
     return {
         "Test": "Test",
-        "sentences": sentence,
-        "embeddings": embeddings[0].tolist()
     }
