79257593934eb9ef381b338adb70072c26cd4e6a
Vegard Harper <vegard.harper@folq.no>
2023-09-12 10:55:18 +0200

    Add another test endpoint

--- api/index.py
+++ api/index.py
@@ -5,9 +5,14 @@ from langchain.llms import OpenAI
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def home():
     llm = OpenAI(temperature=0.9)
     text = "What would be a good company name a company that makes colorful socks?"
 
     return llm(text)
+
+
+@app.route('/hello', methods=['GET'])
+def home2():
+    return 'hello world'
